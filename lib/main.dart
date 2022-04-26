c5a95b400a58f76b8763773aacacc56265383f0f
Erfanmah7 <efn8mah@yahoo.com>
2022-04-27 00:47:12 +0430

    weather project 13

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:p_13_weather/screens/home_screen.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
